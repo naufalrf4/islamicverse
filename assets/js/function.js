bc478ba5730f4fc7cb9b309947efc2eab0abba5f
NRF <contact.admin@naufalrf.me>
2024-01-08 21:26:23 +0700

    not a first commid
    :D

---
+++ assets/js/function.js
@@ -0,0 +1,249 @@
+const endpointUrl = "https://api-berita-indonesia.vercel.app/republika/islam/";
+
+async function fetchLatestPosts() {
+  try {
+    const response = await fetch(endpointUrl);
+    const data = await response.json();
+
+    if (data && data.data && data.data.posts) {
+      const postsContainer = document.getElementById("posts-container");
+
+      data.data.posts.slice(0, 3).forEach((post) => {
+        const postElement = document.createElement("div");
+        postElement.className = "col-md-4";
+        postElement.innerHTML = `
+    <div class="post">
+      <div class="post-img"> 
+        <img class="img-responsive" src="${post.thumbnail}" alt="${
+          post.title
+        }"/> 
+      </div>
+      <div class="post-info">
+        <h3><a href="${post.link}" target="_blank">${post.title}</a></h3>
+        <p>${post.description}</p>
+        <div class="date">${formatDate(post.pubDate)}</div>
+        <a class="readmore" href="${post.link}" target="_blank">
+          <span>Baca Selengkapnya <i class="eicon mdi mdi-arrow-right"></i></span>
+        </a>
+      </div>
+    </div>
+  `;
+
+        postsContainer.appendChild(postElement);
+      });
+    } else {
+      console.error("Invalid data format from API");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(dateString).toLocaleDateString("en-US", options);
+}
+
+fetchLatestPosts();
+const defaultLatitude = -6.2088;
+const defaultLongitude = 106.8456;
+
+function getLocationName(latitude, longitude) {
+  const apiKey = "c780bbdac1d64f1cab2832c6f971a5da";
+  const locationElement = document.getElementById("location");
+
+  if (latitude === defaultLatitude && longitude === defaultLongitude) {
+    locationElement.textContent = " Jakarta";
+  } else {
+    const apiUrl = `https://api.opencagedata.com/geocode/v1/json?key=${apiKey}&q=${latitude}+${longitude}&pretty=1`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results.length > 0) {
+          const locationName = data.results[0].formatted;
+          locationElement.textContent = ` ${locationName}`;
+        } else {
+          locationElement.textContent = "Tidak dapat menemukan nama lokasi.";
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        locationElement.textContent = "Gagal mengambil nama lokasi.";
+      });
+  }
+}
+function getJadwalSholatByLocation(latitude, longitude) {
+  const apiEndpoint = "http://api.aladhan.com/v1/calendar/";
+  const currentDate = new Date();
+  const date = currentDate.getDate();
+  const month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+  const dayName = [
+    "Minggu,",
+    "Senin,",
+    "Selasa,",
+    "Rabu,",
+    "Kamis,",
+    "Jumat,",
+    "Sabtu,",
+  ];
+  const dayIndex = currentDate.getDay();
+  const dayOfWeek = dayName[dayIndex];
+
+  const namaBulan = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+
+  const dateElement = document.querySelector(".date-line p");
+  dateElement.textContent = `${dayOfWeek} ${date} ${
+    namaBulan[month - 1]
+  } ${year}`;
+
+  fetch(
+    `${apiEndpoint}${year}/${month}?latitude=${latitude}&longitude=${longitude}&method=2`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const jadwalSholat = data.data[0].timings;
+      document.getElementById("fajr-counter").textContent = jadwalSholat.Fajr;
+      document.getElementById("dhuhr-counter").textContent = jadwalSholat.Dhuhr;
+      document.getElementById("asr-counter").textContent = jadwalSholat.Asr;
+      document.getElementById("maghrib-counter").textContent =
+        jadwalSholat.Maghrib;
+      document.getElementById("isha-counter").textContent = jadwalSholat.Isha;
+      document.getElementById("fajr-table").textContent = jadwalSholat.Fajr;
+      document.getElementById("dhuhr-table").textContent = jadwalSholat.Dhuhr;
+      document.getElementById("asr-table").textContent = jadwalSholat.Asr;
+      document.getElementById("maghrib-table").textContent =
+        jadwalSholat.Maghrib;
+      document.getElementById("isha-table").textContent = jadwalSholat.Isha;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      const sholatTodayElement = document.getElementById("sholat-today");
+      sholatTodayElement.textContent = "Gagal mengambil data jadwal sholat.";
+    });
+}
+
+document
+  .getElementById("fetch-prayer-times-btn")
+  .addEventListener("click", function () {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          getLocationName(latitude, longitude);
+          getJadwalSholatByLocation(latitude, longitude);
+        },
+        function (error) {
+          console.error("Error getting location:", error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported.");
+    }
+  });
+
+getLocationName(defaultLatitude, defaultLongitude);
+getJadwalSholatByLocation(defaultLatitude, defaultLongitude);
+
+document
+  .getElementById("fetch-prayer-times-btn")
+  .addEventListener("click", function () {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          getLocationName(latitude, longitude);
+          getJadwalSholatByLocation(latitude, longitude);
+        },
+        function (error) {
+          console.error("Error getting location:", error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported.");
+    }
+  });
+async function askServer(question) {
+  try {
+    const responseContainer = document.getElementById("chat-messages");
+    const newMessage = document.createElement("div");
+    newMessage.classList.add("message", "right");
+    newMessage.innerText = question;
+    responseContainer.appendChild(newMessage);
+
+    const endpoint = "https://api.naufalrf.tech/chat";
+
+    const data = {
+      chatId: generateChatId(),
+      message: question,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(endpoint, options);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const serverResponse = await response.json();
+    const answer = serverResponse.content;
+
+    const serverMessage = document.createElement("div");
+    serverMessage.classList.add("message", "left");
+    serverMessage.innerText = answer;
+    responseContainer.appendChild(serverMessage);
+  } catch (error) {
+    console.error("Terdapat kesalahan dalam menjawab pertanyaan:", error);
+  }
+}
+
+const sendButton = document.getElementById("send-button");
+
+sendButton.addEventListener("click", function () {
+  const chatInput = document.getElementById("chat-input");
+  const userQuestion = chatInput.value;
+
+  if (userQuestion.trim() !== "") {
+    askServer(userQuestion);
+    chatInput.value = "";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("send-button").addEventListener("click", () => {
+    const input = document.getElementById("chat-input");
+    const question = input.value.trim();
+    if (question !== "") {
+      askServer(question);
+      input.value = "";
+    }
+  });
+});
+function generateChatId() {
+  return Math.floor(Math.random() * 1000000);
+}
---
+++ assets/js/master.js
@@ -0,0 +1,332 @@
+jQuery(function ($) {
+  "use strict";
+
+  jQuery(document).ready(function ($) {
+    $(".counter").counterUp({
+      delay: 20,
+      time: 5000,
+    });
+
+    mainSlider();
+    testimonialSlider();
+    testimonialSliderTwo();
+    clientSlider();
+    blogGridSlider();
+    parallaxEffect();
+
+    $(window).on("scroll", function () {
+      progress_bars();
+
+      var theta = ($(window).scrollTop() / 200) % Math.PI;
+      $(
+        ".hexagon-gradient-left, .triangle-light-blue, .virus-box-1, .virus-box-2"
+      ).css({ transform: "rotate(" + theta + "rad)" });
+    });
+
+    function progress_bars() {
+      $(".progress .progress-bar:in-viewport").each(function () {
+        if (!$(this).hasClass("animated")) {
+          $(this).addClass("animated");
+          $(this).width($(this).attr("data-width") + "%");
+        }
+      });
+    }
+
+    function mainSlider() {
+      $(".default-slider").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "0",
+        arrows: false,
+      });
+    }
+
+    /* ~~~ Blog Grid Slider ~~~ */
+    function blogGridSlider() {
+      $(".blog-grid-slider").slick({
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "0",
+      });
+    }
+
+    $("a.page-scroll").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 50,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+      event.preventDefault();
+    });
+
+
+    function parallaxEffect() {
+      $(".parallax-effect").parallax();
+    }
+
+
+    var isMobile = {
+      Android: function () {
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function () {
+        return (
+          isMobile.Android() ||
+          isMobile.BlackBerry() ||
+          isMobile.iOS() ||
+          isMobile.Opera() ||
+          isMobile.Windows()
+        );
+      },
+    };
+
+    jQuery(window).stellar({
+      horizontalScrolling: false,
+      hideDistantElements: true,
+      verticalScrolling: !isMobile.any(),
+      scrollProperty: "scroll",
+      responsive: true,
+    });
+
+
+
+
+    /* ===== CBP PORTFOLIO ===== */
+    $(window).on("load", function () {
+      var wow = new WOW({
+        offset: 100,
+        mobile: false,
+      });
+      wow.init();
+
+      $("#chaos-gallery").cubeportfolio({
+        filters: "#chaos-gallery-filter",
+        layoutMode: "grid",
+        defaultFilter: "*",
+        animationType: "frontRow",
+        gapHorizontal: 0,
+        gapVertical: 0,
+        gridAdjustment: "",
+        mediaQueries: [
+          {
+            width: 1500,
+            cols: 5,
+          },
+          {
+            width: 1100,
+            cols: 4,
+          },
+          {
+            width: 800,
+            cols: 3,
+          },
+          {
+            width: 480,
+            cols: 2,
+          },
+          {
+            width: 320,
+            cols: 1,
+          },
+        ],
+        caption: "overlayBottomAlong",
+        displayType: "bottomToTop",
+        displayTypeSpeed: 300,
+      });
+    });
+
+    /* === magnificPopup === */
+
+    $(".image-lightbox").magnificPopup({
+      type: "image",
+      mainClass: "mfp-fade",
+      removalDelay: 160,
+      fixedContentPos: false,
+      // other options
+    });
+
+    $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+      disableOn: 700,
+      type: "iframe",
+      mainClass: "mfp-fade",
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false,
+    });
+  });
+
+  /* ===== PRELOADER  ===== */
+
+  $(window).on("load", function () {
+    // Page loader
+    $("#loader-overlay").delay(500).fadeOut();
+    $(".loader").delay(1000).fadeOut("slow");
+
+    $(window).trigger("scroll");
+    $(window).trigger("resize");
+
+    if (window.location.hash) {
+      var hash_offset = $(window.location.hash).offset().top;
+      $("html, body").animate({
+        scrollTop: hash_offset,
+      });
+    }
+  });
+
+  /* ===== GOOGLE MAPS  ===== */
+
+  /* ~~~ Default Map ~~~ */
+  if ($("#myMap").length > 0) {
+    var $latitude = 42.008315,
+      $longitude = -88.163807,
+      $map_zoom = 12,
+      $marker_url = "assets/images/pin.png",
+      style = [
+        {
+          featureType: "all",
+          elementType: "geometry.fill",
+          stylers: [{ weight: "2.00" }],
+        },
+        {
+          featureType: "all",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#9c9c9c" }],
+        },
+        {
+          featureType: "all",
+          elementType: "labels.text",
+          stylers: [{ visibility: "on" }],
+        },
+        {
+          featureType: "landscape",
+          elementType: "all",
+          stylers: [{ color: "#f2f2f2" }],
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#ffffff" }],
+        },
+        {
+          featureType: "landscape.man_made",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#ffffff" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "all",
+          stylers: [{ visibility: "on" }],
+        },
+        {
+          featureType: "road",
+          elementType: "all",
+          stylers: [{ saturation: -100 }, { lightness: 45 }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#eeeeee" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#7b7b7b" }],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#ffffff" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "all",
+          stylers: [{ visibility: "simplified" }],
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "labels.icon",
+          stylers: [{ visibility: "on" }],
+        },
+        {
+          featureType: "transit",
+          elementType: "all",
+          stylers: [{ visibility: "on" }],
+        },
+        {
+          featureType: "water",
+          elementType: "all",
+          stylers: [{ color: "#46bcec" }, { visibility: "on" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry.fill",
+          stylers: [{ color: "#c8d7d4" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#070707" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#ffffff" }],
+        },
+      ],
+      map_options = {
+        center: new google.maps.LatLng($latitude, $longitude),
+        zoom: $map_zoom,
+        panControl: !0,
+        zoomControl: !0,
+        mapTypeControl: !0,
+        streetViewControl: !0,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: !1,
+        styles: style,
+      },
+      map = new google.maps.Map(document.getElementById("myMap"), map_options),
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng($latitude, $longitude),
+        map: map,
+        visible: !0,
+        icon: $marker_url,
+      }),
+      contentString = '<div id="mapcontent"><p>Chaos</p></div>',
+      infowindow = new google.maps.InfoWindow({
+        maxWidth: 320,
+        content: contentString,
+      });
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.open(map, marker);
+    });
+  }
+});
+
+/*End Jquery*/
