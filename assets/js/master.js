55898e565d380cf71762b57ca4eb11e6f9854c49
NRF <contact.admin@naufalrf.me>
2024-01-09 23:04:06 +0700

    updated 0.1.2

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,3 +1,12 @@
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
 jQuery(function ($) {
   "use strict";
 
